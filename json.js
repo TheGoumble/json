a734dd143f0e251da9d1de032f05160258b90011
Javier Vargas <thebruh@Javiers-MacBook-Air.local>
2022-07-15 17:24:27 -0400

    using json forever now

---
+++ json.js
@@ -0,0 +1,15 @@
+const data = require('./data.json')
+
+//data destructoring 
+const {tas, teachers} = data
+
+console.log('The Tas are:')
+for(let i = 0; i < tas.length; i++){
+    let ta = tas[i]
+    console.log(ta.name)
+}
+
+console.log('The Teachers are:')
+for(let i = 0; i < teachers.length; i++){
+    console.log(teachers[i].name)
+}
